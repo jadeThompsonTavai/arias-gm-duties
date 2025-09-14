42bdc0bb4318c35a70167da73bc94369994e629a
Jade Thompson-Tavai <jade.t.tavai@gmail.com>
2025-09-14 12:39:18 +1200

    added sprites and sprite placements
    - updated fonts to change colour with mode
    - added placeholder for gifs
    - set layout
    - set font

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { relative } from "path";
+
+import workBtn from "./assets/work-blue.png";
+import workBtnClicked from "./assets/work-blue-clicked.png";
+import breakBtn from "./assets/break-blue.png";
+import breakBtnClicked from "./assets/break-blue-clicked.png";
+import closeBtn from "./assets/close.png";
+import playImg from "./assets/play.png";
+import resetImg from "./assets/reset.png";
+
 
 function App() {
   const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [isRunning, setIsRunning] = useState(false);
   const [isBreak, setIsBreak] = useState(false);
   const [encouragement, setEncouragement] = useState("");
+  const [workButtonImage, setWorkButtonImage] = useState(workBtn);
+  const [breakButtonImage, setBreakButtonImage] = useState(breakBtn);
+  const [image, setImage] = useState(playImg);
 
   const workMessages = [
     "Is this all you got?",
@@ -35,7 +46,7 @@ function App() {
       messageInterval = setInterval(() => {
         setEncouragement(messages[index]);
         index = (index + 1) % messages.length;
-      }, 4000);
+      }, 4000); //4 sec interval
     } else {
       setEncouragement("");
     }
@@ -68,6 +79,13 @@ function App() {
 
   const switchMode = (breakMode: boolean) => {
     setIsBreak(breakMode);
+    if(!breakMode){
+      setWorkButtonImage(workBtnClicked);
+      setBreakButtonImage(breakBtn);
+    } else {
+      setWorkButtonImage(workBtn);
+      setBreakButtonImage(breakBtnClicked);
+    }
     setIsRunning(false);
     setTimeLeft(breakMode ? 5 * 60 : 25 * 60);
   };
@@ -81,31 +99,45 @@ function App() {
     }
   };
 
+ 
+  const containerClass = `home-container ${isBreak ? "bg-blue" : ""}`;
+  const timerClass = `home-timer ${isBreak ? "timer-blue": ""}`;
+
+
   return (
-    <div style={{ position: "relative" }}>
+    <div className={containerClass} style={{ position: "relative" }}>
       <div>
-        <button className="closeButton">Close</button>
+        <button className="closeButton">
+          <img src={closeBtn} alt="close"/>
+        </button>
       </div>
 
       <div className="home-content">
         <div className="home-controls">
-          <button className="image-button" onClick={() => switchMode(false)}>
-            Work
+          <button className="image-button" id="work-button" onClick={() => switchMode(false)}>
+            <img src={workButtonImage} alt="work" />
           </button>
-          <button className="image-button" onClick={() => switchMode(true)}>
-            Break
+          <button className="image-button" id="break-button" onClick={() => switchMode(true)}>
+            <img src={breakButtonImage} alt="break" />
           </button>
         </div>
-      </div>
 
-      {/* Here will display working animations instead of encouragement --> Can have character dialogue instead */}
-      <p className={`encouragement-text ${isRunning ? "hidden" : ""}`}>{encouragement}</p>
+          {/* Here will display working animations instead of encouragement --> Can have character dialogue instead */}
+      <p className={`encouragement-text ${isRunning ? "hidden" : ""} ${isBreak ? "break-text" : ""}`}>
+        {encouragement}
+      </p>
 
-      <h1 className="home-timer">{formatTime(timeLeft)}</h1>
+      
+      <h1 className={timerClass}>{formatTime(timeLeft)}</h1>
+      <img src={closeBtn} alt="Timer Status" className="gif-image"/><p>Will be replaced with gifs</p>
 
       <button className="home-button" onClick={handleClick}>
-        Start
+        <img src={playImg} />
       </button>
+      </div>
+
+    
+
     </div>
   );
 }
